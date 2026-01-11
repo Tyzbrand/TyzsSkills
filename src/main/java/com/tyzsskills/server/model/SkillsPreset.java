a92d6402b8e5227894c03f9519e9290e75ecb0c0
Tyzbrand <martin.frot32@gmail.com>
2026-01-11 10:26:07 +0100

    Addes Abilities preset

--- src/main/java/com/tyzsskills/server/model/SkillsPreset.java
+++ src/main/java/com/tyzsskills/server/model/SkillsPreset.java
@@ -11,20 +11,20 @@ public class SkillsPreset {
     public static List<Skill> GetDefaultSkills(){
         List<Skill> finalList = new ArrayList<>();
 
-        finalList.addAll(GetAbilitieSkills());
+        finalList.addAll(GetAbilitySkills());
         finalList.addAll(GetFightSkills());
 
         return finalList;
     }
 
-    private static List<Skill> GetAbilitieSkills(){
+    private static List<Skill> GetAbilitySkills(){
         List<Skill> finalList = new ArrayList<>();
 
         finalList.add(new Skill(
                 true,
                 "Health_boost",
                 10,
-                List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                List.of(2, 2, 3, 3, 5, 5, 7, 7, 9, 9),
                 List.of(2.0f, 4.0f, 6.0f, 8.0f, 10.0f, 12.0f, 14.0f, 16.0f, 18.0f, 20.0f),
                 Skill.SkillType.GENERIC,
                 MainGUI.CategoryType.ABILITIES,
@@ -34,6 +34,111 @@ public class SkillsPreset {
                 "tyzs_skills:textures/gui/skills/health_boost.png",
                 "skill.tyzs_skills.health_boost.displayName",
                 "skill.tyzs_skills.health_boost.description"));
+
+        finalList.add(new Skill(
+                true,
+                "Oxygen_boost",
+                4,
+                List.of(3, 5, 7, 9),
+                List.of(1.25f, 1.50f, 1.75f, 2f),
+                Skill.SkillType.GENERIC,
+                MainGUI.CategoryType.ABILITIES,
+                "minecraft:generic.oxygen_bonus",
+                AttributeModifier.Operation.ADD_MULTIPLIED_BASE,
+                true,
+                "tyzs_skills:textures/gui/skills/oxygene_boost.png",
+                "skill.tyzs_skills.oxygen_boost.displayName",
+                "skill.tyzs_skills.oxygen_boost.description"));
+
+        finalList.add(new Skill(
+                true,
+                "Block_reach",
+                4,
+                List.of(3, 5, 9, 13),
+                List.of(1f, 2f, 3f, 4f),
+                Skill.SkillType.GENERIC,
+                MainGUI.CategoryType.ABILITIES,
+                "minecraft:player.block_interaction_range",
+                AttributeModifier.Operation.ADD_VALUE,
+                true,
+                "tyzs_skills:textures/gui/skills/block_reach.png",
+                "skill.tyzs_skills.block_reach.displayName",
+                "skill.tyzs_skills.block_reach.description"));
+
+        finalList.add(new Skill(
+                true,
+                "Attack_damage",
+                3,
+                List.of(5, 10, 16),
+                List.of(1f, 2f, 3f),
+                Skill.SkillType.GENERIC,
+                MainGUI.CategoryType.ABILITIES,
+                "minecraft:generic.attack_damage",
+                AttributeModifier.Operation.ADD_VALUE,
+                true,
+                "tyzs_skills:textures/gui/skills/attack_damage.png",
+                "skill.tyzs_skills.attack_damage.displayName",
+                "skill.tyzs_skills.attack_damage.description"));
+
+        finalList.add(new Skill(
+                true,
+                "Sneaking_speed",
+                5,
+                List.of(1, 2, 3, 4, 5),
+                List.of(1.2f, 1.4f, 1.6f, 1.8f, 2f),
+                Skill.SkillType.GENERIC,
+                MainGUI.CategoryType.ABILITIES,
+                "minecraft:player.sneaking_speed",
+                AttributeModifier.Operation.ADD_MULTIPLIED_BASE,
+                true,
+                "tyzs_skills:textures/gui/skills/sneaking_speed.png",
+                "skill.tyzs_skills.sneaking_speed.displayName",
+                "skill.tyzs_skills.sneaking_speed.description"));
+
+        finalList.add(new Skill(
+                true,
+                "Speed_boost",
+                5,
+                List.of(2, 4, 6, 8, 11),
+                List.of(1.2f, 1.4f, 1.6f, 1.8f, 2f),
+                Skill.SkillType.GENERIC,
+                MainGUI.CategoryType.ABILITIES,
+                "minecraft:generic.movement_speed",
+                AttributeModifier.Operation.ADD_MULTIPLIED_BASE,
+                true,
+                "tyzs_skills:textures/gui/skills/speed.png",
+                "skill.tyzs_skills.speed.displayName",
+                "skill.tyzs_skills.speed.description"));
+
+        finalList.add(new Skill(
+                true,
+                "Step_height",
+                2,
+                List.of(3, 5),
+                List.of(1f, 2f),
+                Skill.SkillType.GENERIC,
+                MainGUI.CategoryType.ABILITIES,
+                "minecraft:generic.step_height",
+                AttributeModifier.Operation.ADD_VALUE,
+                true,
+                "tyzs_skills:textures/gui/skills/step_height.png",
+                "skill.tyzs_skills.step_height.displayName",
+                "skill.tyzs_skills.step_height.description"));
+
+        finalList.add(new Skill(
+                true,
+                "Swim_speed",
+                5,
+                List.of(2, 4, 6, 8, 11),
+                List.of(1.2f, 1.4f, 1.6f, 1.8f, 2f),
+                Skill.SkillType.GENERIC,
+                MainGUI.CategoryType.ABILITIES,
+                "neoforge:swim_speed",
+                AttributeModifier.Operation.ADD_MULTIPLIED_BASE,
+                true,
+                "tyzs_skills:textures/gui/skills/swim_speed.png",
+                "skill.tyzs_skills.swim_speed.displayName",
+                "skill.tyzs_skills.swim_speed.description"));
 
         return finalList;
     }
